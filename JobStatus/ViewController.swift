f30d0d23bf64b89ee84e4825d1df237845cc95ee
Rimil Dey <rimildeyjsr@gmail.com>
2018-04-19 09:52:02 +0530

    Initial Commit

---
+++ JobStatus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JobStatus
+//
+//  Created by Rimil Dey on 19/04/18.
+//  Copyright © 2018 Rimil Dey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
