41dc85101d3b9e0af020eb88de2546c4495411ea
Rimil Dey <rimildeyjsr@gmail.com>
2018-04-19 10:00:37 +0530

    added files for table view controller and table view cell

---
+++ JobStatus/MainTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainTableViewCell.swift
+//  JobStatus
+//
+//  Created by Rimil Dey on 19/04/18.
+//  Copyright © 2018 Rimil Dey. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
