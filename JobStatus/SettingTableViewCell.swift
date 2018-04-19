8fe69d0432d5280e78bf9e4ea4d8343572ec9d0d
Rimil Dey <rimildeyjsr@gmail.com>
2018-04-19 13:56:07 +0530

    ui fot settings done

---
+++ JobStatus/SettingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingTableViewCell.swift
+//  JobStatus
+//
+//  Created by Rimil Dey on 19/04/18.
+//  Copyright © 2018 Rimil Dey. All rights reserved.
+//
+
+import UIKit
+
+class SettingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
