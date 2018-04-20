ec0bd907a36227928282cc73a57329433290dd65
Rimil Dey <rimildeyjsr@gmail.com>
2018-04-20 10:18:22 +0530

    keyboard resigns when done button is pressed

--- JobStatus/NewEntryViewController.swift
+++ JobStatus/NewEntryViewController.swift
@@ -8,28 +8,29 @@
 
 import UIKit
 
-class NewEntryViewController: UIViewController {
+class NewEntryViewController: UIViewController, UITextFieldDelegate {
 
+    // MARK: - view did load
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        setupTextField()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - outlets
+    @IBOutlet weak var companyTextfield: UITextField!
+    @IBOutlet weak var sourceTextfield: UITextField!
+    @IBOutlet weak var positionTextfield: UITextField!
+    @IBOutlet var textFields: [UITextField]!
+    
+    func setupTextField() {
+        for textField in textFields {
+            textField.delegate = self
+        }
     }
-    */
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 
 }
